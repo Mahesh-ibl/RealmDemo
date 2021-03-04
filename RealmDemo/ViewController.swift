aa35081b9cb84ad3a2d36c544bd12fbf1a2d691f
Mahesh-ibl <mahesh.rathod@iblinfotech.com>
2021-03-04 15:33:15 +0530

    Initial Commit

---
+++ RealmDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmDemo
+//
+//  Created by IBL INFOTECH on 04/03/21.
+//  Copyright © 2021 IBL INFOTECH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
